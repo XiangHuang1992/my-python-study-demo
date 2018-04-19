c5106b77fa1ae65e55509eb315293c5f45085d53
huangxiang <huangxiang1992@hotmail.com>
2018-04-19 22:52:32 +0800

    移除django demo

--- hx_django/hx_blog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HxBlogConfig(AppConfig):
-    name = 'hx_blog'
--- hx_django/hx_blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    # hx_blog
-    url(r'^$', views.index, name='index'),
-    # hx_blog/712
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-]
